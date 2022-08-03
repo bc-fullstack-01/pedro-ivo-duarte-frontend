4687cbce1fa63e3c6c2d7d609c584ccd57c1dee8
Peu <peustratt@gmail.com>
2022-08-03 00:25:41 -0300

    request feed posts

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,6 +1,25 @@
+import { useEffect } from "react";
 import CustomAppBar from "../../components/CustomAppBar";
+import server from "../../api/server";
 
 const Home = () => {
+  useEffect(() => {
+    const token = localStorage.getItem("acessToken");
+    const getPosts = async () => {
+      try {
+        const response = await server.get("/feed", {
+          headers: {
+            authorization: `Bearer ${token}`
+          }
+        });
+        console.log(response.data);
+      } catch (err) {
+        alert(err);
+      }
+    };
+    getPosts();
+  }, []);
+
   return (
     <div>
       <CustomAppBar title="home" />
