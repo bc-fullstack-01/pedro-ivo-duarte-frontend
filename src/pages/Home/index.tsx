cab2fd0f99140993a20ed13d7863d07d1b69fe2c
Peu <peustratt@gmail.com>
2022-08-04 02:16:42 -0300

    added react-infinite-scroll-component lib

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,35 +1,43 @@
 import { useState, useEffect } from "react";
+import InfiniteScroll from "react-infinite-scroll-component";
 import { Divider } from "@mui/material";
+
 import CustomAppBar from "../../components/CustomAppBar";
 import PostCard from "../../components/PostCard";
 import server from "../../api/server";
-
 import { Post } from "../../models/Post";
 
 const Home = () => {
-  const [posts, setPosts] = useState<Post[]>(() => []);
   const token = localStorage.getItem("accessToken");
+  const [posts, setPosts] = useState<Post[]>(() => []);
+  const [page, setPage] = useState<number>(0);
 
   useEffect(() => {
+    console.log("useEffect");
     const getPosts = async () => {
       try {
-        const response = await server.get("/feed", {
+        const response = await server.get(`/feed?page=${page}`, {
           headers: {
             authorization: `Bearer ${token}`,
           },
         });
-        setPosts(response.data);
-        console.log(response.data);
+        setPosts([...posts, ...response.data]);
       } catch (err) {
         alert(err);
       }
     };
     getPosts();
-  }, [token]);
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [token, page]);
+
+  const loadMorePosts = async () => {
+    console.log("more!", page);
+    setPage((prevPage) => prevPage + 1);
+  };
 
   const postsEl = posts.map((post) => (
     <>
-      <PostCard post={post} />
+      <PostCard key={post._id} post={post} />
       <Divider />
     </>
   ));
@@ -37,7 +45,16 @@ const Home = () => {
   return (
     <div>
       <CustomAppBar title="home" />
-      <div style={{ marginTop: "56px" }}>{postsEl}</div>
+      <div style={{ marginTop: "56px" }}>
+        <InfiniteScroll
+          dataLength={posts.length}
+          next={loadMorePosts}
+          hasMore={true}
+          loader={<h4>Loading...</h4>}
+        >
+          {postsEl}
+        </InfiniteScroll>
+      </div>
     </div>
   );
 };
