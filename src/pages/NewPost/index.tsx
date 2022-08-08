d46f2c20c964792f0bd86baa854f9f16fa3503ae
peustratt <peustratt@gmail.com>
2022-08-08 20:32:34 -0300

    NewPost page setup

--- src/pages/NewPost/index.tsx
+++ src/pages/NewPost/index.tsx
@@ -1,10 +1,30 @@
+import { Button, Container, Stack, TextField } from "@mui/material";
 import CustomAppBar from "../../components/CustomAppBar";
 
 const NewPost = () => {
   return (
     <div>
       <CustomAppBar title="Novo Post" />
-      <h1 style={{ marginTop: "100px" }}>Feed</h1>
+      <Container sx={{marginTop: '88px'}}>
+        <Stack spacing={6}>
+          <TextField 
+            variant="standard" 
+            label="Título" 
+            name="title" 
+          />
+          <TextField
+            variant="standard"
+            label="O que está acontecendo?"
+            name="description"
+            multiline
+            minRows={2}
+
+          />
+          <Button type="submit" variant="contained">
+            Publicar
+          </Button>
+        </Stack>
+      </Container>
     </div>
   );
 };
