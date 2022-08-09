37c1aa92448abafce213e7a776f251cbc32e160b
peustratt <peustratt@gmail.com>
2022-08-09 18:10:12 -0300

    implemented socket.io-client on CustomAppBar for notifications

--- src/components/CustomAppBar/index.tsx
+++ src/components/CustomAppBar/index.tsx
@@ -1,4 +1,7 @@
-import { AppBar, Toolbar, Typography, Box } from "@mui/material";
+import { useState, useEffect } from "react";
+import io from "socket.io-client";
+import { AppBar, Toolbar, Typography, Box, Badge } from "@mui/material";
+import { Email as EmailIcon } from "@mui/icons-material";
 import { useNavigate } from "react-router-dom";
 import {
   Home as HomeIcon,
@@ -7,6 +10,7 @@ import {
   AccountCircle as AccountCircleIcon,
 } from "@mui/icons-material";
 import CustomIconButton from "../CustomIconButton";
+import { SERVER_ADRESS } from "../../constants";
 
 interface Props {
   title: string;
@@ -14,6 +18,61 @@ interface Props {
 
 const CustomAppBar = ({ title }: Props) => {
   const navigate = useNavigate();
+  const token = localStorage.getItem("accessToken");
+  const [messageCount, setMessageCount] = useState(0);
+
+  const socket = io(SERVER_ADRESS, {
+    auth: { token },
+  });
+
+  useEffect(() => {
+    socket.on("connected", () => {
+      console.log(socket);
+    });
+
+    socket.on("connected_profile", (profile) => {
+      console.log(profile);
+    });
+
+    socket.on("disconnected", () => {
+      console.log(socket);
+    });
+
+    socket.on("post", (data) => {
+      console.log(data);
+      setMessageCount((count) => count + 1);
+    });
+
+    socket.on("post-like", (data) => {
+      console.log(data);
+      setMessageCount((count) => count + 1);
+    });
+
+    socket.on("comment", (data) => {
+      console.log(data);
+      setMessageCount((count) => count + 1);
+    });
+
+    socket.on("comment-like", (data) => {
+      console.log(data);
+      setMessageCount((count) => count + 1);
+    });
+
+    socket.on("connect_error", (err) => {
+      console.error(err);
+    });
+
+    return () => {
+      socket.off();
+    };
+  }, [token, socket]);
+
+  const handleClickEmail = () => {
+    if (messageCount) {
+      setMessageCount(0);
+      window.location.reload();
+    }
+  };
 
   return (
     <AppBar position="fixed">
@@ -28,16 +87,38 @@ const CustomAppBar = ({ title }: Props) => {
         </Typography>
         <Box sx={{ flexGrow: 1 }} />
         <Box sx={{ display: { xs: "none", md: "flex" } }}>
-          <CustomIconButton label="show home" onClickCallBack={() => navigate("/home")}>
+          <CustomIconButton
+            label="show home"
+            onClickCallBack={() => navigate("/home")}
+          >
             <HomeIcon />
           </CustomIconButton>
-          <CustomIconButton label="show edit" onClickCallBack={() => {navigate('/create')}}>
+          <CustomIconButton
+            label="notifications"
+            onClickCallBack={handleClickEmail}
+          >
+            <Badge badgeContent={messageCount} color="secondary">
+              <EmailIcon />
+            </Badge>
+          </CustomIconButton>
+          <CustomIconButton
+            label="show edit"
+            onClickCallBack={() => {
+              navigate("/create");
+            }}
+          >
             <EditIcon />
           </CustomIconButton>
-          <CustomIconButton label="show profiles" onClickCallBack={() => navigate("/profiles")}>
+          <CustomIconButton
+            label="show profiles"
+            onClickCallBack={() => navigate("/profiles")}
+          >
             <GroupIcon />
           </CustomIconButton>
-          <CustomIconButton label="show profile" onClickCallBack={() => navigate("/profile")}>
+          <CustomIconButton
+            label="show profile"
+            onClickCallBack={() => navigate("/profile")}
+          >
             <AccountCircleIcon />
           </CustomIconButton>
         </Box>
