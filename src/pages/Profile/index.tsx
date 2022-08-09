4654a3a9ba6528a1aef1518bf23f71e5a9f30aad
peustratt <peustratt@gmail.com>
2022-08-09 15:26:33 -0300

    implemented profile page

--- src/pages/Profile/index.tsx
+++ src/pages/Profile/index.tsx
@@ -1,10 +1,23 @@
+import { Box, Button } from "@mui/material";
+import { useNavigate } from "react-router-dom";
 import CustomAppBar from "../../components/CustomAppBar";
 
 const Profile = () => {
+  const navigate = useNavigate();
+  const name = localStorage.getItem("user");
+  const handleLogout = () => {
+    localStorage.clear();
+    navigate("/");
+  };
   return (
     <div>
       <CustomAppBar title="Perfil" />
-      <h1 style={{ marginTop: "100px" }}>Feed</h1>
+      <Box component="main" sx={{ flexGrow: 1, p: 3, marginTop: 8 }}>
+        <h1>{name}</h1>
+        <Button variant="contained" onClick={handleLogout}>
+          Sair
+        </Button>
+      </Box>
     </div>
   );
 };
