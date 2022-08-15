1d9e018826f8bdf241ad1b4eae7275ffc6e63d4e
peustratt <peustratt@gmail.com>
2022-08-15 01:36:33 -0300

    setup server on localhost

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,4 @@
-const SERVER_ADRESS = "http://192.168.0.58:4000/v1";
-// const SERVER_ADRESS='http://localhost:4000/v1'
+// const SERVER_ADRESS = "http://192.168.0.58:4000/v1";
+const SERVER_ADRESS='http://localhost:4000/v1'
 
 export { SERVER_ADRESS };
