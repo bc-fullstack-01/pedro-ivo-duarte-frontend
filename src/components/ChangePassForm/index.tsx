defd4536192c8fcd4494103d772b97b356fde91a
peustratt <peustratt@gmail.com>
2022-08-14 23:56:11 -0300

    -feature password check error messages

--- src/components/ChangePassForm/index.tsx
+++ src/components/ChangePassForm/index.tsx
@@ -1,57 +1,137 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, ReactElement } from "react";
 import { useNavigate } from "react-router-dom";
 import { Stack, TextField, Button } from "@mui/material";
 import server from "../../api/server";
 
+interface Password {
+  password: {
+    value: string;
+    error: string;
+  };
+  confirmPassword: {
+    value: string;
+    error: string;
+  };
+}
+
 const ChangePassForm = ({ token }: { token: string }) => {
-  const [passwd, setPasswd] = useState({ value: "", error: "" });
-  const [confirmPasswd, setConfirmPasswd] = useState({ value: "", error: "" });
+  const [input, setInput] = useState({
+    password: {
+      value: "",
+      error: "",
+    },
+    confirmPassword: {
+      value: "",
+      error: "",
+    },
+  });
+
+  const [error, setError] = useState({
+    password: "",
+    confirmPassword: "",
+  });
   const navigate = useNavigate();
 
-  const handleChangePasswd = async (e: React.FormEvent<HTMLFormElement>) => {
+  const handleOnSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    console.log(passwd.value);
-    if (passwd.value === confirmPasswd.value) {
+    if (
+      input.password.value &&
+      input.confirmPassword.value &&
+      !error.password &&
+      !error.confirmPassword
+    ) {
+      const { password } = input;
       try {
         const response = await server.put(
           `/users/me`,
-          { password: passwd.value },
+          { password: password.value },
           {
             headers: {
               authorization: `Bearer ${token}`,
             },
           }
         );
-        console.log(response.data);
         localStorage.clear();
         navigate("/");
       } catch (err) {
         alert(err);
       }
-    } else {
-      alert("As senhas precisam ser iguais!");
     }
   };
 
+  const handleInputChange = (e: any) => {
+    const { name, value } = e.target;
+    setInput((prevState) => ({
+      ...prevState,
+      [name]: {
+        value,
+        error: "",
+      },
+    }));
+    validateInput(e);
+  };
+
+  const validateInput = ({ target }: any) => {
+    let { name, value } = target;
+
+    setError((prevState) => {
+      const stateObj = { ...prevState, [name]: "" };
+
+      switch (name) {
+        case "password":
+          if (!value) {
+            stateObj["password"] = "Por favor digite uma senha.";
+          } else if (
+            input.confirmPassword.value &&
+            value !== input.confirmPassword.value
+          ) {
+            stateObj.confirmPassword = "A senhas digitadas não coincidem";
+          }
+          break;
+        case "confirmPassword":
+          if (!value) {
+            stateObj["confirmPassword"] =
+              "Por favor digite a senha de confirmação";
+          } else if (input.password.value && value !== input.password.value) {
+            stateObj["confirmPassword"] = "A senhas digitadas não coincidem";
+          }
+          break;
+        default:
+          break;
+      }
+
+      return stateObj;
+    });
+  };
+
+  const { password, confirmPassword } = input;
+
   return (
-    <form onSubmit={handleChangePasswd}>
+    <form onSubmit={handleOnSubmit}>
       <Stack spacing={2}>
         <TextField
           variant="outlined"
           label="Nova senha"
           type="password"
-          value={passwd.value}
-          onChange={(e) => setPasswd({ value: e.target.value, error: "" })}
+          name="password"
+          value={password.value}
+          onChange={handleInputChange}
+          onBlur={validateInput}
         />
+        {error.password && <span className="err">{error.password}</span>}
+
         <TextField
           variant="outlined"
           label="Confirmar nova senha"
           type="password"
-          value={confirmPasswd.value}
-          onChange={(e) =>
-            setConfirmPasswd({ value: e.target.value, error: "" })
-          }
+          name="confirmPassword"
+          value={confirmPassword.value}
+          onChange={handleInputChange}
+          onBlur={validateInput}
         />
+        {error.confirmPassword && (
+          <span className="err">{error.confirmPassword}</span>
+        )}
         <Button variant="outlined" type="submit">
           Mudar senha!
         </Button>
